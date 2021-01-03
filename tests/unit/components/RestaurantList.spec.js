7299d99530937fe590de3bf3498787cfece05154
Niek <17721120+Niekvan@users.noreply.github.com>
2021-01-03 16:07:15 +0000

    Load restaurants upon mounting RestaurantList

---
+++ tests/unit/components/RestaurantList.spec.js
@@ -0,0 +1,24 @@
+import { createStore } from 'vuex';
+import { mount } from '@vue/test-utils';
+import RestaurantList from '@/components/RestaurantList';
+
+describe('RestaurantList', () => {
+  it('loads restaurants on mount', () => {
+    const restaurantsModule = {
+      namespaced: true,
+      actions: {
+        load: jest.fn().mockName('load'),
+      },
+    };
+
+    const store = new createStore({
+      modules: {
+        restaurants: restaurantsModule,
+      },
+    });
+
+    mount(RestaurantList, { global: { plugins: [store] } });
+
+    expect(restaurantsModule.actions.load).toHaveBeenCalled();
+  });
+});
